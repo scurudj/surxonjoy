cde3f9904025d80dd20cf9afc7a7e576f29e077a
scurudj <shuxratsaloxiddinov620@gmail.com>
2026-07-13 15:36:06 +0500

    Install Tailwind CSS and create reusable UI components

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+
+      <h1 className="text-6xl font-bold text-blue-600">
+        SurxonJoy
+      </h1>
+
+    </div>
+  );
+}
